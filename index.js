2f5ba255086230c51b2fc0a285d8132590a1abd9
arturshevchenko <shevchenkoarchi@gmail.com>
2023-04-07 20:27:26 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Code your solution in this file!
+// Code your solution in this fil e!
+ 
+ const returnFirstTwoDrivers = function(driversArray) {
+    let drivers = [...driversArray.slice(0, 2)]
+    return drivers
+}
+    const returnLastTwoDrivers = function(driversArray) {
+        let drivers = [...driversArray.slice(2,4)]
+        return drivers
+    }
+    const selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers]
+    function createFareMultiplier(intg){
+        return function(fare = 4) {
+            return fare * intg
+        }
+    } 
+    const fareDoubler = function(fare){
+        return (fare * 2) }
+        const fareTripler = function(fare){
+            return (fare * 3)
+        }
+        function selectDifferentDrivers(driversArray, fn) {
+            return fn(driversArray)
+        }
+    
